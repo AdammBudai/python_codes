1e60352a192ecb3c84ed5800437bc963e7623cf2
AdammBudai <133277237+AdammBudai@users.noreply.github.com>
2023-05-11 23:03:07 +0200

    Add files via upload

---
+++ erastothenes_sieve.py
@@ -0,0 +1,17 @@
+import sys
+
+def sito(n):
+    is_prime = [False,False] + [True]*(n-1)
+    prime_numbers = []
+    for i in range(2,n+1):
+        if is_prime[i] == True:
+            prime_numbers.append(i)
+            for j in range(i**2,n+1,i):
+                is_prime[j] = False
+    return len(prime_numbers)
+
+for line in sys.stdin:
+    if len(line.strip().split()) > 0:
+        line = int(line)
+        print(sito(line))
+        break
